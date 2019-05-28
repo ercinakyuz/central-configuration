7e7f4b14994e1a16b04b65c38210ffb5e3a09d5e
eakyuz <ercin.akyuz@hepsiburada.com>
2019-05-28 12:53:31 +0300

    RabbitMq Connection Attempts 3 Times

--- CentralConfiguration.MessageBroker/RabbitMqConnectionManager.cs
+++ CentralConfiguration.MessageBroker/RabbitMqConnectionManager.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using RabbitMQ.Client;
 
 namespace CentralConfiguration.MessageBroker
@@ -17,7 +18,37 @@ namespace CentralConfiguration.MessageBroker
                     Uri = new Uri(connnectionString)
                 };
             }
-            return _connection ?? (_connection = _connectionFactory.CreateConnection());
+
+            TryConnect();
+            return _connection;
+        }
+
+        private static void TryConnect()
+        {
+            int attemptLimit = 3;
+
+            for (int i = 0; i < attemptLimit; i++)
+            {
+                Thread.Sleep(1000);
+                try
+                {
+                    if (_connection == null || !_connection.IsOpen)
+                    {
+                        _connection = _connectionFactory.CreateConnection();
+                    }
+
+                    if (_connection != null && _connection.IsOpen)
+                    {
+                        return;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    
+                }
+            }
+
         }
     }
 }
